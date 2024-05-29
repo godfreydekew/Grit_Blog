19750ba062eca388b24968ffe7e318875d8c2df2
godfreydekew <dekewgodfrey@gmail.com>
2024-05-29 12:14:11 +0300

    Add create button to index.html

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Integer, String, Text
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, URL
