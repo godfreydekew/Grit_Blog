d50b7b3ecc8ba1170a8c62ddfbc088876376365a
godfreydekew <dekewgodfrey@gmail.com>
2024-05-29 10:59:55 +0300

    Create database using SQLAlchemy

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, redirect, url_for, request
+from flask_bootstrap import Bootstrap5
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, URL
+from flask_ckeditor import CKEditor, CKEditorField
+import datetime
 import requests
 import smtplib
+
+
 my_email = "dekewgodfrey@gmail.com"
 password = "xibegzoydgxlerka"
 
@@ -8,8 +17,27 @@ password = "xibegzoydgxlerka"
 posts = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
 
 app = Flask(__name__)
+ckeditor = CKEditor(app)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+Bootstrap5(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+db = SQLAlchemy()
+db.init_app(app)
 
 
+class BlogPost(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    subtitle = db.Column(db.String(250), nullable=False)
+    date = db.Column(db.String(250), nullable=False)
+    body = db.Column(db.Text, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    img_url = db.Column(db.String(250), nullable=False)
+
+
+with app.app_context():
+    db.create_all()
 @app.route('/')
 def get_all_posts():
     return render_template("index.html", all_posts=posts)
