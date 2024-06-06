d91fe6a6dab9016cab858bd2f16a1db80ea0a223
godfreydekew <dekewgodfrey@gmail.com>
2024-06-06 21:30:05 +0300

    Clean up

--- forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired, URL
-
-class RegisterForm(FlaskForm):
-    email = StringField("Email")
-    name = StringField("Name")
-    password = StringField("Password")
